57c5c4e060d3c8b9c8be128658958875e27e3a67
g13947sf <g13947sf@s30455.tsuda.ac.jp>
2016-04-22 12:16:23 +0900

    ラベル変更

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -150,7 +150,7 @@ public class Anagrams extends JFrame {
 
         guessLabel.setDisplayedMnemonic('Y');
         guessLabel.setLabelFor(guessedWord);
-        guessLabel.setText("Your Guess:");
+        guessLabel.setText("予想:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
